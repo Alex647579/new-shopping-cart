fe7034943ca24e6bc6624929b3ee12bf6936c2aa
Alex Lu Wang <alexluwang2019@u.northwestern.edu>
2019-02-04 23:07:01 -0600

    closes #3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     ));
 
     return (
-      <div>
+      <div className="catalog">
         {productsList}
       </div>
     );
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import '../App.css'
 
 class Product extends Component {
   render() {
     // TODO product picture
     let productData = this.props.data;
+    let productImgSrc = '/products/' + productData['sku'] + '_1.jpg';
     let productName = productData['title'];
     let productPrice = productData['price'];
 
-    console.log(productData);
     return (
-      <div>
-        <div></div>
-        <div>{productName}</div>
-        <div>{productPrice}</div>
+      <div className="productWrapper">
+        <img className="productImage" img src={productImgSrc} alt="Product"></img>
+        <div className="productName">{productName}</div>
+        <div className="productPrice">{productPrice}</div>
         <div><button>Add To Cart</button></div>
       </div>
     );
